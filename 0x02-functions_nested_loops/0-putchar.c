64a932e290e1d6ec96f0a59b954efff04b265d3e
Eyram87 <eyramatsiavor@gmail.com>
2023-03-21 01:59:56 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints main
+ *
+ * Description: prints main
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char text[10] = "main";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
